a6ac98bc76037acb7c304217e6ee53adc21d3437
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-19 20:04:20 +0530

    Created a utility function to establish connection to local mongodb database

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import config from "config";
+import connect from "./utils/connect";
+
+const port = config.get<number>('port')
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
     console.log(`Listening on PORT: ${3000}`)
+    await connect();
 })
