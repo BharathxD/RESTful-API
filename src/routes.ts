b35122aecbc6b3ae2b5d07d6f5039e14071831ff
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-21 14:26:06 +0530

    Added all the required routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,26 @@
 import { Express, Request, Response } from "express";
 import { createUserHandler } from "./controller/user.controller";
-import validate from "./middleware/validateResource";
 import { createUserSchema } from "./schema/user.schema";
+import createSessionSchema from "./schema/session.schema";
+import requireUser from "./middleware/requireUser";
+import validate from "./middleware/validateResource";
 import {
   createUserSessionHandler,
   deleteSessionHandler,
   getUserSessionHandler,
 } from "./controller/session.controller";
-import createSessionSchema from "./schema/session.schema";
-import requireUser from "./middleware/requireUser";
+import {
+  createProductHandler,
+  deleteProductHandler,
+  getProductHandler,
+  updateProductHandler,
+} from "./controller/product.controller";
+import {
+  createProductSchema,
+  deleteProductSchema,
+  getProductSchema,
+  updateProductSchema,
+} from "./schema/product.schema";
 
 const routes = (app: Express) => {
   app.get("/healthcheck", (req: Request, res: Response) => {
@@ -22,6 +34,30 @@ const routes = (app: Express) => {
   );
   app.get("/api/sessions", requireUser, getUserSessionHandler);
   app.delete("/api/sessions", requireUser, deleteSessionHandler);
+
+  app.get(
+    "/api/products/:productId",
+    validate(getProductSchema),
+    getProductHandler
+  );
+
+  app.post(
+    "/api/products",
+    [requireUser, validate(createProductSchema)],
+    createProductHandler
+  );
+
+  app.put(
+    "/api/products/:productId",
+    [requireUser, validate(updateProductSchema)],
+    updateProductHandler
+  );
+
+  app.delete(
+    "/api/products/:productId",
+    [requireUser, validate(deleteProductSchema)],
+    deleteProductHandler
+  );
 };
 
 export default routes;
