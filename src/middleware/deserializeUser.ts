bb51595f17e5404fe95023e0e33dc51897e5b080
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-08 23:08:37 +0530

    Added a try-catch block to handle errors gracefully.

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -8,38 +8,48 @@ const deserializeUser = async (
   res: Response,
   next: NextFunction
 ) => {
-  const accessToken = get(req, "headers.authorization", "").replace(
-    /^Bearer\s/,
-    ""
-  );
+  try {
+    const accessToken = get(req, "headers.authorization", "").replace(
+      /^Bearer\s/,
+      ""
+    );
 
-  const refreshToken = get(req, "header.x-refresh");
+    const refreshToken = get(req, "headers.x-refresh");
 
-  if (!accessToken) {
-    return next();
-  }
-
-  const { decoded, expired } = verifyJwt(accessToken, "accessTokenPublicKey");
-
-  if (decoded) {
-    res.locals.user = decoded;
-    return next();
-  }
-
-  if (expired && refreshToken) {
-    const newAccessToken = await reIssueAccessToken({ refreshToken });
-
-    if (newAccessToken) {
-      res.setHeader("x-access-token", newAccessToken);
+    if (!accessToken) {
+      return next();
     }
 
-    const result = verifyJwt(newAccessToken as string, "accessTokenPublicKey");
+    const { decoded, expired } = verifyJwt(accessToken, "accessTokenPublicKey");
 
-    res.locals.user = result.decoded;
+    if (decoded) {
+      res.locals.user = decoded;
+      return next();
+    }
+
+    if (expired && refreshToken) {
+      const newAccessToken = await reIssueAccessToken({
+        refreshToken: refreshToken.toString(),
+      });
+
+      if (newAccessToken) {
+        res.setHeader("x-access-token", newAccessToken);
+      }
+
+      const result = verifyJwt(
+        newAccessToken as string,
+        "accessTokenPublicKey"
+      );
+
+      res.locals.user = result.decoded;
+      return next();
+    }
+
     return next();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ message: "Something went wrong" });
   }
-
-  return next();
 };
 
 export default deserializeUser;
